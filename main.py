7acbd83c8d800cffd923666b99b57180bbf2644e
JByrd11 <101782245+JByrd11@users.noreply.github.com>
2025-01-16 19:25:19 +0000

    2 Game Loop

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import pygame, sys
-from pygame.locals import QUIT
+import pygame
+from constants import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
-while True:
-   for event in pygame.event.get():
-       if event.type == QUIT:
-           pygame.quit()
-           sys.exit()
-   pygame.display.update()
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
