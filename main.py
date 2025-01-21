8de64f12100dd52aed38c8e2a14f03abe476ccd6
JByrd11 <101782245+JByrd11@users.noreply.github.com>
2025-01-21 02:14:16 +0000

    Groups

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from constants import *
 from player import Player
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,9 +20,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         ms = clock.tick(60)
         dt = ms / 1000
